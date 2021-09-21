e9b22f03c526be4c2f1dc25e896523653c10cbed
torebjerkan <tbj034@post.uit.no>
2021-09-21 19:30:38 +0200

    adding a controller for blog

---
+++ Blog/Controllers/BlogController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Models.Entities;
+
+namespace Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: BlogController
+        public ActionResult Index()
+        {
+            List<Models.Entities.Blog> blogs = new()
+            {
+                new Models.Entities.Blog {BlogId = 1, Name = "First in line", ClosedForPosts = false},
+                new Models.Entities.Blog {BlogId = 2, Name = "Everything was great", ClosedForPosts = false},
+            };
+            return View(blogs);
+        }
+
+        // GET: BlogController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BlogController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BlogController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BlogController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BlogController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BlogController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BlogController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
