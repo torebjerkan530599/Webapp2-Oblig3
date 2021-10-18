50b0ac19e50e5aa9b354ccad7fb073530ca6ea4e
torebjerkan <tbj034@post.uit.no>
2021-10-18 20:17:02 +0200

    små endringer på html

--- Blog/wwwroot/js/site.js
+++ Blog/wwwroot/js/site.js
@@ -2,6 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/*
 const uri = "api/CommentsWebApi";
 let todos = [];
 
@@ -129,4 +130,4 @@ function _displayItems(data) {
     });
 
     todos = data;
-}
+}*/
